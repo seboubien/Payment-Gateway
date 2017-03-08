be2c0582d51bb2b7c4a428c131ed974c4cfe5eb1
seboubien <sebastienoubien@gmail.com>
2017-03-08 22:58:28 +0100

    Update pay-per-post.php

--- Examples/pay-per-post.php
+++ Examples/pay-per-post.php
@@ -20,8 +20,8 @@
 	$amountUSD		= 0.5;				// price per one post - 0.5 USD
 	$period			= "NOEXPIRY";		// one time payment for each new user post, not expiry
 	$def_language	= "en";				// default Payment Box Language
-	$public_key		= "-your public key for coin box-"; // from gourl.io
-	$private_key	= "-your private key for coin box-";// from gourl.io
+	$public_key		= "9138AAowi9RBitcoin77BTCPUBqbJXIZuIgPUY7Ly89xywSBA0"; // from gourl.io
+	$private_key	= "9138AAowi9RBitcoin77BTCPRVSDOgAmm5W3yX5x0HnZWf0my4";// from gourl.io
 
 	// IMPORTANT: Please read description of options here - https://gourl.io/api-php.html#options  
 	
